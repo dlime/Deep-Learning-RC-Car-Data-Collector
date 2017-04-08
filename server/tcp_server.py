2fd419e95ec15b065ba0fd13ba5327ebf7d0c6df
Dario Limongi <dario.limongi@gmail.com>
2017-04-08 17:59:02 +0200

    Integrate data record loop with server routine
    TODO:
    * would be nicer to use threads (dunno how long it will take for refactor)

--- server/tcp_server.py
+++ server/tcp_server.py
@@ -5,6 +5,16 @@ import car_dir
 import motor
 from socket import *
 from time import ctime  # Import necessary modules
+import cv2
+import csv
+
+video_capture = cv2.VideoCapture(0)
+csv_file = open('IMG/driving_log.csv', 'w', newline='')
+writer = csv.writer(csv_file)
+image_counter = 0
+loop_counter = 0
+recording_enabled = False
+current_steering_angle = 0
 
 ctrl_cmd = ['forward', 'backward', 'left', 'right', 'stop', 'read cpu_temp', 'home', 'distance', 'x+', 'x-', 'y+', 'y-',
             'xy_home', 'toggleRecordTrue', 'toggleRecordFalse']
@@ -28,6 +38,17 @@ video_dir.home_x_y()
 car_dir.home()
 
 
+def record_data():
+    global image_counter
+    if recording_enabled and loop_counter % 200 == 0:
+        success, image = video_capture.read()
+        if success:
+            image_path = "IMG/central-" + str(image_counter) + ".jpg"
+            writer.writerow([image_path, current_steering_angle])
+            cv2.imwrite(image_path, image)
+            image_counter += 1
+
+
 def setup():
     global tcpCliSock
     print 'Waiting for connection...'
@@ -38,101 +59,115 @@ def setup():
     print '...connected from :', addr  # Print the IP address of the client connected with the server.
 
 
+def process_command(data):
+    global recording_enabled, current_steering_angle
+    if data == ctrl_cmd[0]:
+        print 'motor moving forward'
+        motor.forward()
+    elif data == ctrl_cmd[1]:
+        print 'recv backward cmd'
+        motor.backward()
+    elif data == ctrl_cmd[2]:
+        print 'recv left cmd'
+        car_dir.turn_left()
+    elif data == ctrl_cmd[3]:
+        print 'recv right cmd'
+        car_dir.turn_right()
+    elif data == ctrl_cmd[6]:
+        print 'recv home cmd'
+        car_dir.home()
+    elif data == ctrl_cmd[4]:
+        print 'recv stop cmd'
+        motor.ctrl(0)
+    elif data == ctrl_cmd[5]:
+        print 'read cpu temp...'
+        temp = cpu_temp.read()
+        tcpCliSock.send('[%s] %0.2f' % (ctime(), temp))
+    elif data == ctrl_cmd[8]:
+        print 'recv x+ cmd'
+        video_dir.move_increase_x()
+    elif data == ctrl_cmd[9]:
+        print 'recv x- cmd'
+        video_dir.move_decrease_x()
+    elif data == ctrl_cmd[10]:
+        print 'recv y+ cmd'
+        video_dir.move_increase_y()
+    elif data == ctrl_cmd[11]:
+        print 'recv y- cmd'
+        video_dir.move_decrease_y()
+    elif data == ctrl_cmd[12]:
+        print 'home_x_y'
+        video_dir.home_x_y()
+    elif data == ctrl_cmd[13]:
+        print 'toggleRecordTrue'
+        recording_enabled = True
+    elif data == ctrl_cmd[14]:
+        print 'toggleRecordFalse'
+        recording_enabled = False
+    elif data[0:5] == 'speed':  # Change the speed
+        print data
+        num_len = len(data) - len('speed')
+        if num_len == 1 or num_len == 2 or num_len == 3:
+            tmp = data[-num_len:]
+            print 'tmp(str) = %s' % tmp
+            spd = int(tmp)
+            print 'spd(int) = %d' % spd
+            if spd < 24:
+                spd = 24
+            motor.setSpeed(spd)
+    elif data[0:6] == 'turnBy':
+        print data
+        num_len = len(data) - len('turnBy')
+        if num_len == 1 or num_len == 2 or num_len == 3:
+            received_angle = data[-num_len:]
+            steering_angle = int(received_angle)
+            print 'steering_angle(int) = %d' % steering_angle
+            current_steering_angle = steering_angle
+            car_dir.turn_by(steering_angle)
+    elif data[0:5] == 'turn=':  # Turning Angle
+        print 'data =', data
+        angle = data.split('=')[1]
+        try:
+            angle = int(angle)
+            car_dir.turn(angle)
+        except:
+            print 'Error: angle =', angle
+    elif data[0:8] == 'forward=':
+        print 'data =', data
+        spd = data[8:]
+        try:
+            spd = int(spd)
+            motor.forward(spd)
+        except:
+            print 'Error speed =', spd
+    elif data[0:9] == 'backward=':
+        print 'data =', data
+        spd = data.split('=')[1]
+        try:
+            spd = int(spd)
+            motor.backward(spd)
+        except:
+            print 'ERROR, speed =', spd
+    else:
+        print 'Command Error! Cannot recognize command: ' + data
+
+
 def loop():
+    global loop_counter
     while True:
-        data = ''
-        data = tcpCliSock.recv(BUFSIZ)  # Receive data sent from the client.
+        # Receive data sent from the client.
+        data = tcpCliSock.recv(BUFSIZ)
+
         # Analyze the command received and control the car accordingly.
-        if not data:
-            break
-        if data == ctrl_cmd[0]:
-            print 'motor moving forward'
-            motor.forward()
-        elif data == ctrl_cmd[1]:
-            print 'recv backward cmd'
-            motor.backward()
-        elif data == ctrl_cmd[2]:
-            print 'recv left cmd'
-            car_dir.turn_left()
-        elif data == ctrl_cmd[3]:
-            print 'recv right cmd'
-            car_dir.turn_right()
-        elif data == ctrl_cmd[6]:
-            print 'recv home cmd'
-            car_dir.home()
-        elif data == ctrl_cmd[4]:
-            print 'recv stop cmd'
-            motor.ctrl(0)
-        elif data == ctrl_cmd[5]:
-            print 'read cpu temp...'
-            temp = cpu_temp.read()
-            tcpCliSock.send('[%s] %0.2f' % (ctime(), temp))
-        elif data == ctrl_cmd[8]:
-            print 'recv x+ cmd'
-            video_dir.move_increase_x()
-        elif data == ctrl_cmd[9]:
-            print 'recv x- cmd'
-            video_dir.move_decrease_x()
-        elif data == ctrl_cmd[10]:
-            print 'recv y+ cmd'
-            video_dir.move_increase_y()
-        elif data == ctrl_cmd[11]:
-            print 'recv y- cmd'
-            video_dir.move_decrease_y()
-        elif data == ctrl_cmd[12]:
-            print 'home_x_y'
-            video_dir.home_x_y()
-        elif data == ctrl_cmd[13]:
-            print 'toggleRecordTrue'
-            # TODO
-        elif data == ctrl_cmd[14]:
-            print 'toggleRecordFalse'
-            # TODO
-        elif data[0:5] == 'speed':  # Change the speed
-            print data
-            numLen = len(data) - len('speed')
-            if numLen == 1 or numLen == 2 or numLen == 3:
-                tmp = data[-numLen:]
-                print 'tmp(str) = %s' % tmp
-                spd = int(tmp)
-                print 'spd(int) = %d' % spd
-                if spd < 24:
-                    spd = 24
-                motor.setSpeed(spd)
-        elif data[0:6] == 'turnBy':
-            print data
-            numLen = len(data) - len('turnBy')
-            if numLen == 1 or numLen == 2 or numLen == 3:
-                received_angle = data[-numLen:]
-                steering_angle = int(received_angle)
-                print 'steering_angle(int) = %d' % steering_angle
-                car_dir.turn_by(steering_angle)
-        elif data[0:5] == 'turn=':  # Turning Angle
-            print 'data =', data
-            angle = data.split('=')[1]
-            try:
-                angle = int(angle)
-                car_dir.turn(angle)
-            except:
-                print 'Error: angle =', angle
-        elif data[0:8] == 'forward=':
-            print 'data =', data
-            spd = data[8:]
-            try:
-                spd = int(spd)
-                motor.forward(spd)
-            except:
-                print 'Error speed =', spd
-        elif data[0:9] == 'backward=':
-            print 'data =', data
-            spd = data.split('=')[1]
-            try:
-                spd = int(spd)
-                motor.backward(spd)
-            except:
-                print 'ERROR, speed =', spd
-        else:
-            print 'Command Error! Cannot recognize command: ' + data
+        if data:
+            process_command(data)
+
+        if recording_enabled:
+            record_data()
+
+        # This is required so that we store an image every 200 loops and avoid data spam
+        # TODO with a separate recorder thread this could have been done with time.sleep
+        loop_counter += 1
 
 
 if __name__ == "__main__":
@@ -141,3 +176,5 @@ if __name__ == "__main__":
         loop()
     except KeyboardInterrupt:
         tcpSerSock.close()
+        csv_file.close()
+        video_capture.release()
